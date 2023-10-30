56da89f19283a90fc5902d042b4786bdb5f742ef
Sumin Lee <sule@itu.dk>
2023-10-30 20:35:33 +0100

    initial commit

---
+++ CurrencyConverter/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Net.Http;
+
+namespace CurrencyConverter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    class CurrencyConverter
+    {
+        public void importer()
+        {
+            
+        }
+    }
+}
